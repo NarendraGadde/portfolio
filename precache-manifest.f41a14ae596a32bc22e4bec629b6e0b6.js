dc48f979853687b2dddfa5ec2d80e51dca67a06d
Narendra Gadde <narendragadde18@gmail.com>
2021-09-07 16:10:47 +0530

    Updates

---
+++ precache-manifest.f41a14ae596a32bc22e4bec629b6e0b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "643c6ac9c87e1f6c84fa",
+    "url": "/portfolio/static/css/main.569fb74d.chunk.css"
+  },
+  {
+    "revision": "643c6ac9c87e1f6c84fa",
+    "url": "/portfolio/static/js/main.643c6ac9.chunk.js"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "1c3bccd22e4eec3e27da",
+    "url": "/portfolio/static/js/2.1c3bccd2.chunk.js"
+  },
+  {
+    "revision": "1f72300a341dd152b99e4dd49f318f8d",
+    "url": "/portfolio/index.html"
+  }
+];

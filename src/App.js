b10c517a32432a2dec251d1e89b6ef541e9acb0e
Narendra Gadde <narendragadde18@gmail.com>
2021-09-07 13:35:08 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React, { Component } from 'react';
-import './App.css';
-import Sidebar from './components/sidebar'
-import Introduction from './components/introduction'
-import About from './components/about'
-import Timeline from './components/timeline'
-import Photography from './components/photography'
-import Blog from './components/blog'
-import ContactMe from './components/contactme';
+//Main File
+import React, { Component } from "react";
+import "./App.css";
+import Sidebar from "./components/sidebar";
+import Introduction from "./components/introduction";
+import About from "./components/about";
+import Timeline from "./components/timeline";
+import Photography from "./components/photography";
+import Blog from "./components/blog";
+import ContactMe from "./components/contactme";
 
 class App extends Component {
   render() {
     return (
       <div id="colorlib-page">
         <div id="container-wrap">
-         	<Sidebar />
+          <Sidebar />
           <div id="colorlib-main">
             <Introduction />
             <About />
@@ -22,7 +23,7 @@ class App extends Component {
             <Blog />
             <ContactMe />
           </div>
-      	</div>
+        </div>
       </div>
     );
   }
